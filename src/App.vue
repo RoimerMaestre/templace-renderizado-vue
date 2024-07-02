afedf12d43957d256d1cefa8b984736b05d27729
RoimerMaestre <roimer.maestre93@gmail.com>
2024-07-02 18:30:15 -0400

    se crea repositorio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FormularioInteractivo/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import FormularioInteractivo from './components/formularioInteractivo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioInteractivo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/formularioInteractivo.vue
@@ -0,0 +1,112 @@
+<template>
+  <section class="flexBox">
+    <form class="formulario">
+      <label for="input1">Color de fondo:</label>
+      <input v-model="fondo" type="text" id="input1" name="input1" /><br /><br />
+
+      <label for="input2">color de texto</label>
+      <input v-model="color" type="text" id="input2" name="input2" /><br /><br />
+
+      <label>
+        Mostrar texto
+        <input type="checkbox" id="input3" name="input3" v-model="mostrarTexto" /> </label><br /><br />
+
+      <label for="input4">Borde</label>
+      <input type="range" id="input4" name="input4" v-model="borde" min="0" max="200" /><br /><br />
+
+      <label for="input5">Contenido actual</label>
+      <input v-model="texto" type="text" id="input5" name="input5" maxlength="35" /><br /><br />
+
+      <label>
+      
+        Tipografía:
+        <select id="input6" name="input6" v-model="tipoLetra">
+          <option v-for="fuente in fuentes" :key="fuente" :value="fuente">{{ fuente }}</option>
+        </select> 
+
+        <!-- <select id="input6" name="input6" v-model="tipoLetra">
+          <option value="arial">Arial</option>
+          <option value="cursiva">Cursiva</option>
+          <option value="impact">impact</option>
+          <option value="garamond">Garamond</option>
+        </select>  </label><br /><br /> -->
+      </label><br /><br />
+
+      <label>
+        Opacidad
+        <input type="checkbox" id="input7" name="input7" v-model="opacity"/> </label><br /><br />
+
+      <label>
+        Tamaño de letra
+        <input  v-model=" tamanoLetra" type="radio" id="pequenio" name="tamano" value="pequenio" />
+        <label  for="pequenio">Pequeño</label>
+        <input  v-model=" tamanoLetra" type="radio" id="mediano" name="tamano" value="mediano" />
+        <label for="mediano">Mediano</label>
+        <input  v-model=" tamanoLetra" type="radio" id="grande" name="tamano" value="grande" />
+        <label for="grande">Grande</label> </label><br /><br />
+    </form>
+
+    <div :class=" {'opacidad':opacity === true }" 
+     :style="{
+      fontFamily: tipoLetra,
+      color: color,
+      backgroundColor: fondo,
+      borderRadius: borde + 'px',
+      fontSize: tamanoLetra === 'pequenio' ? '12px' : tamanoLetra === 'mediano' ? '16px' : '20px',
+    }" class="cuadroDinamico">
+      <h1 v-if="mostrarTexto">{{ texto }}</h1>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fondo: "",
+      texto: "",
+      color: "",
+      mostrarTexto: true,
+       tipoLetra: "",
+      fuentes: ['Arial', 'Cursiva', 'Impact', 'Garamond'], // Arreglo para el v-for
+      borde: 0,
+      opacity:"",
+      tamanoLetra:"",
+
+    }
+  }
+
+};
+</script>
+
+<style>
+.formulario {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  width: 25%;
+  background-color: cadetblue;
+}
+
+.flexBox {
+  display: flex;
+  justify-content: space-around
+}
+
+.cuadroDinamico {
+  width: 400px;
+  height: 400px;
+  border: solid;
+  text-align: center;
+  line-height: 350px;
+
+}
+
+.fondo {
+  background-color: blue;
+}
+.opacidad{
+  opacity: 0.3;
+}
+</style>
